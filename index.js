98e198f576bafea07520a1b4704dd966c54bc08c
Dmg5th <dmgould310@gmail.com>
2021-01-18 11:13:50 -0500

    commit tree bbb5d5f0a8cd0ec90b381580f8ac29ebe71386d4

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+function map(array, callBack){
+    return array.map((i) => {
+        return callBack(i)
+    })
+}
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
+
